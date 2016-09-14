2e187610f709813f456c89024b3b4effc4bea550
HomeEmail <591694604@qq.com>
2016-09-14 17:22:52 +0800

    Signed-off-by: HomeEmail <591694604@qq.com>

--- res/js/main.js
+++ res/js/main.js
@@ -262,12 +262,12 @@ function detailTabInitShow(){
 
 ;(function() {
 	wx.config({
-		debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+		debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
 		//appId: 'wx92d82a6e0fd6ff09', // 必填，公众号的唯一标识
 		timestamp: '1459303663', // 必填，生成签名的时间戳
 		nonceStr: 'pYvCP2V0sUQLtXTM', // 必填，生成签名的随机串
 		signature: '',// 必填，签名，见附录1
-		jsApiList: ['onMenuShareTimeline', 'onMenuShareAppMessage'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+		jsApiList: ['onMenuShareTimeline', 'onMenuShareAppMessage','scanQRCode'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
 	});
 	var loginWxBaseUrl = 'http://wechat.utvmall.cn/wechat/wechat-goLogin/GoLogin?rqType=Login&after_login_url=';
 	wx.ready(function () {
